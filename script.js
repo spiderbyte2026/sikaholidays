485a925f699494b09d3504d58574a76a7bd7e736
spiderbyte2026 <spiderbyte2026@gmail.com>
2026-05-19 00:37:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -24,3 +24,45 @@
             }
         });
     });
+    const navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+     const counts = document.querySelectorAll('.count-number');
+    let counted = false;
+
+    const startCounters = () => {
+        counts.forEach(count => {
+            const target = +count.getAttribute('data-target');
+            const duration = 2000; // ms
+            const increment = target / (duration / 16); // 60fps
+            let current = 0;
+
+            const updateCounter = () => {
+                current += increment;
+                if (current < target) {
+                    count.innerText = Math.ceil(current);
+                    requestAnimationFrame(updateCounter);
+                } else {
+                    count.innerText = target;
+                }
+            };
+            updateCounter();
+        });
+    };
+
+    const countsObserver = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && !counted) {
+            startCounters();
+            counted = true;
+        }
+    }, { threshold: 0.5 });
+
+    const countSection = document.querySelector('.count-grid');
+    if (countSection) {
+        countsObserver.observe(countSection);
+    }
